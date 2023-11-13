b9c35cfeed889cd8180f9f5f7f3b412f04a0732e
unknown <amitsingh17051@gmail.com>
2023-11-13 16:06:06 +0530

    Update blog index, main index and menu urls

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
+		tags: z.array(z.string()),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
